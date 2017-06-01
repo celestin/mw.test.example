0271c7301d79fa4f4a0e9bf24056e1d74fbaccdd
klemens-morgenstern <klemens.morgenstern@gmx.net>
2017-06-01 23:19:39 +0200

    added travis.mk

---
+++ stm32f4-discovery/build/config-travis.mk
@@ -0,0 +1,5 @@
+MW-TEST-PATH := $/mw.test
+GCC-PATH := $/gcc-arm-none-eabi-6-2017-q1-update/bin/
+GCC-PREFIX := arm-none-eabi-
+BOOST-LOCATION := $/boost
+QEMU := $/qemu/2.8.0-201612271623-dev/bin/qemu-system-gnuarmeclipse
