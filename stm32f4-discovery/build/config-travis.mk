4eef38b5f69613aaf8aad680a0a404d0116babe3
klemens-morgenstern <klemens.morgenstern@gmx.net>
2017-06-01 23:23:22 +0200

    added build commands

--- stm32f4-discovery/build/config-travis.mk
+++ stm32f4-discovery/build/config-travis.mk
@@ -1,5 +1,5 @@
-MW-TEST-PATH := $/mw.test
-GCC-PATH := $/gcc-arm-none-eabi-6-2017-q1-update/bin/
+MW-TEST-PATH := /home/travis/mw.test
+GCC-PATH := /home/travis/gcc-arm-none-eabi-6-2017-q1-update/bin/
 GCC-PREFIX := arm-none-eabi-
-BOOST-LOCATION := $/boost
-QEMU := $/qemu/2.8.0-201612271623-dev/bin/qemu-system-gnuarmeclipse
+BOOST-LOCATION := /home/travis/boost
+QEMU := /home/travis/qemu/2.8.0-201612271623-dev/bin/qemu-system-gnuarmeclipse
