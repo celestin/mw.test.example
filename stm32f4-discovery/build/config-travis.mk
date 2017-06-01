f5cbf876d78e9550a9e9f3ca2db3c63ce2a517ac
klemens-morgenstern <klemens.morgenstern@gmx.net>
2017-06-02 00:16:04 +0200

    fixed boost path

--- stm32f4-discovery/build/config-travis.mk
+++ stm32f4-discovery/build/config-travis.mk
@@ -1,5 +1,5 @@
 MW-TEST-PATH := /home/travis/mw.test/bin/release
 GCC-PATH := /home/travis/gcc-arm-none-eabi-6-2017-q1-update/bin/
 GCC-PREFIX := arm-none-eabi-
-BOOST-LOCATION := /home/travis/boost
+BOOST-LOCATION := /home/travis/boost_1_64_0
 QEMU := /home/travis/qemu/2.8.0-201612271623-dev/bin/qemu-system-gnuarmeclipse
